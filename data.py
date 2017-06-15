7cfaa9a976ef19355fff14f07a96f8bdcba18e9b
timofey1198 <timostar98@gmail.com>
2017-06-15 11:12:55 +0300

    Добавил две функции get_link и get_status Добавил тело функции new_user

--- data.py
+++ data.py
@@ -25,7 +25,15 @@ def get_last_link_num():
     return cursor.fetchall()[0][0]
 
 def new_user(chat_id, link):
-    pass
+    number = get_last_link_num()
+    if number == None:
+        number = 0
+    number += 1
+    conn = sqlite3.connect(main_path + '/data/links.db')
+    cursor = conn.cursor()
+    cursor.execute("""INSERT INTO news
+                   VALUES (?, ?, ?, ?)
+                   """, [str(number), str(chat_id), 'work', link])    
 
 def update_link(chat_id, new_link):
     pass
@@ -35,6 +43,12 @@ def new_status(chat_id, status):
     Change link status
     available statuses: {'work', 'closed', 'found'}
     '''
+    pass
+
+def get_link(num):
+    pass
+
+def get_starus(num):
     pass
 
 
