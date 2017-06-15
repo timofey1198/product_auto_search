4ac7481282b35cc70558c534bc38561b1f32ffa2
timofey1198 <timostar98@gmail.com>
2017-06-15 11:38:43 +0300

    Прописаны тела всех функций

--- data.py
+++ data.py
@@ -24,34 +24,72 @@ def get_last_link_num():
     cursor.execute(sql)
     return cursor.fetchall()[0][0]
 
-def new_user(chat_id, link):
+def new_user(chat_id, link, status = 'work'):
     number = get_last_link_num()
     if number == None:
         number = 0
     number += 1
     conn = sqlite3.connect(main_path + '/data/links.db')
     cursor = conn.cursor()
-    cursor.execute("""INSERT INTO news
+    cursor.execute("""INSERT INTO links
                    VALUES (?, ?, ?, ?)
-                   """, [str(number), str(chat_id), 'work', link])    
+                   """, [str(number), str(chat_id), status, link])
+    conn.commit()
 
 def update_link(chat_id, new_link):
-    pass
+    conn = sqlite3.connect(main_path + '/data/links.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+    UPDATE links
+    SET link=?
+    WHERE chat_id=?
+    """, [new_link, str(chat_id)])
+    conn.commit()
 
 def new_status(chat_id, status):
     '''
     Change link status
-    available statuses: {'work', 'closed', 'found'}
+    available statuses: {'work', 'closed', 'found', 'test'}
     '''
-    pass
+    conn = sqlite3.connect(main_path + '/data/links.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+    UPDATE links
+    SET status=?
+    WHERE chat_id=?
+    """, [status, str(chat_id)])
+    conn.commit()
+
+def get_link_info(num):
+    last_num = get_last_link_num()
+    if type(num) != int:
+        return None
+    if last_num == None:
+        return None
+    if num > last_num:
+        num = 1
+    conn = sqlite3.connect(main_path + '/data/links.db')
+    cursor = conn.cursor()
+    cursor.execute("""SELECT * FROM links WHERE id=?
+                   """, [("%i"%num)])
+    return cursor.fetchall()[0]
 
 def get_link(num):
-    pass
+    info = get_link_info(num)
+    return info[3]
 
-def get_starus(num):
-    pass
+def get_status(num):
+    info = get_link_info(num)
+    return info[2]
+
+def get_chat_id(num):
+    info = get_link_info(num)
+    return info[1]
 
 
 if __name__ == '__main__':
+    new_status(111111, 'test')
+    print(get_link_info(1))
+    #new_user(111111, 'http://test.test', 'test')
     #print(help(main_search_bot))
-    pass
+    #pass
